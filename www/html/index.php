20ec3db2ae9059f623e589e54b1fc71cb5e9c9dc
Tom Andreas Krane Vingås <tomvingaas@outlook.com>
2020-03-10 15:58:06 +0100

    docker-compose med Docker files for php-miljø

---
+++ www/html/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>  
+    <head>  
+        <title>Hello World!</title>  
+    </head>  
+
+    <body>  
+        <h1>Hello World!</h1>  
+        <p>
+            <?php echo 'We are running PHP, version: ' . phpversion(); ?></p>  
+        <?php  
+        echo "<h1>Testside</h1>"
+        ?>  
+    </body> 
